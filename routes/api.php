667ff4018f0a17ca1084a16ae5e5d6707887770f
Dery Wahyu Perdana <dery.5230411153@student.uty.ac.id>
2024-12-25 14:24:20 +0700

    membuat route middleware

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\ProdukController;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('kategoris', KategoriController::class);
+    Route::apiResource('produks', ProdukController::class);
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
