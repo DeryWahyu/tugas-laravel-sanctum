c2fe0654c6f908f6594cb49bac5e134b6c513ac6
Dery Wahyu Perdana <dery.5230411153@student.uty.ac.id>
2024-12-25 13:48:00 +0700

    membuat autentikasi

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    // form register
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'confirm_password' => 'required|same:password',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Ada kesalahan',
+                'errors' => $validator->errors()
+            ]);
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+
+        $success['token'] = $user->createToken('auth_token')->plainTextToken;
+        $success['name'] = $user->name;
+
+        return response()->json([
+            'success' => true,
+            'data' => $success,
+            'message' => 'Registrasi berhasil'
+        ]);
+    }
+
+    // login
+    public function login(Request $request)
+    {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $auth = Auth::user();
+            $success['token'] = $auth->createToken('auth_token')->plainTextToken;
+            $success['name'] = $auth->name;
+            $success['email'] = $auth->email;
+            // jika ingin menampilkan data lainnya, tambahkan disini
+
+            return response()->json([
+                'success' => true,
+                'data' => $success,
+                'message' => 'Login berhasil'
+            ]);
+
+        } else {
+            return response()->json([
+                'success' => false,
+                'data' => null,
+                'message' => 'email atau password salah'
+            ]);
+        }
+    }
+
+    // Logout
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json([
+            'message' => 'Successfully logged out',
+        ]);
+    }
+}
